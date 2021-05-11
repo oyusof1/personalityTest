bde4279462c309ea03bc6c2794b825fbdc7bc761
Osman Yusof <oyusof01@ivytech.edu>
2021-05-10 22:04:13 -0400

    fixed visual studio

--- main.cpp
+++ main.cpp
@@ -2,11 +2,35 @@
 #include <string>
 #include <fstream>
 #include <vector>
-#include "personality_test.cpp"
 #include "personality_test.h"
-#include "output.cpp"
+
 
 using namespace std;
+
+void printBanner() {
+    cout << "====================================" << endl;
+    cout << "  Welcome to our Personality Test!" << endl;
+    cout << "      12 Questions to Analyze      " << endl;
+    cout << "          Your Personality!         " << endl;
+    cout << "====================================" << endl << endl;
+}
+
+//Prints out instructions
+void printInstructions() {
+    cout << "*Note*" << endl;
+    cout << "Answer each question with \"1\" for true, and \"0\" for false"
+        << " based on your preference." << endl << endl;
+}
+
+bool thankYou() {
+    bool playAgain = false;
+    cout << "  Thank You for Taking Our Test" << endl;
+    cout << " Enter 0 to Take Again or 1 to Exit " << endl;
+    cout << "====================================" << endl << endl;
+
+    cin >> playAgain;
+    return playAgain;
+}
 
 int main() {
 
